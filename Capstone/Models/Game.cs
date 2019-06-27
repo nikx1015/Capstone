2a553ce0b3ccc158f52358509c1f35e771ad3dee
nikx1015 <nikx1015@gmail.com>
2019-06-27 10:12:12 -0400

    added models, may need updating later

---
+++ Capstone/Models/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Game
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string EsrbRating { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        [Required]
+        public string NumberOfPlayers { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public bool HavePlayed { get; set; }
+
+        public virtual ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ Capstone/Models/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Tag
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int Title { get; set; }
+    }
+}
