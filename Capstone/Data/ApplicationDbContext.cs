66c2ad32776ba431e485627d86a61bc272503a31
nikx1015 <nikx1015@gmail.com>
2019-06-27 10:40:54 -0400

    working on identity setup

--- Capstone/Data/ApplicationDbContext.cs
+++ Capstone/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Capstone.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,9 @@ namespace Capstone.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
+
         {
-        }
+    }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
--- Capstone/Startup.cs
+++ Capstone/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using Capstone.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Capstone.Models;
 
 namespace Capstone
 {
@@ -38,7 +39,7 @@ namespace Capstone
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<ApplicationUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
