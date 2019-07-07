c40a0cf99653ac22274c2999cabb499b1754872c
nikx1015 <nikx1015@gmail.com>
2019-07-07 01:13:20 -0400

    database

--- Capstone/Data/Migrations/20190707051209_changes-to-games-description.Designer.cs
+++ Capstone/Data/Migrations/20190707051209_changes-to-games-description.Designer.cs
@@ -4,14 +4,16 @@ using Capstone.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Capstone.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190707051209_changes-to-games-description")]
+    partial class changestogamesdescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
