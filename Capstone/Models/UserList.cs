00ea0307549aad2947fc3c971817772fb7a49d46
nikx1015 <nikx1015@gmail.com>
2019-07-12 20:17:03 -0400

    changes to titles displayed

--- Capstone/Models/UserList.cs
+++ Capstone/Models/UserList.cs
@@ -14,6 +14,7 @@ namespace Capstone.Models
         [Required]
         public int GameId { get; set; }
 
+        [Display(Name = "Saved Game Title")]
         public Game game { get; set; }
 
         [Required]
